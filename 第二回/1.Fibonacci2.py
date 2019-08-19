547db90ead1d29fd2f6f5649d771f6261f248da3
rkhrsm <rikuhiro0729@gmail.com>
2019-08-19 16:20:39 +0900

    no message

---
+++ 第二回/1.Fibonacci2.py
@@ -0,0 +1,12 @@
+def main(n):
+    listA  = [0] * (n + 1)
+    return Fibonacci(n,listA)
+
+def Fibonacci(n,listA):
+    
+
+if __name__ == "__main__":
+    n = int(input("n = "))
+    print(main(n))
+
+##Fibonacciを埋めてください
