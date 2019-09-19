0492f199d80c061c4773a37eddf02feab636bb9d
rkhrsm <rikuhiro0729@gmail.com>
2019-09-19 09:22:28 +0900

    第4回

--- 第四回/threading1.py
+++ 第四回/threading1.py
@@ -10,17 +10,19 @@ def Double():
     while True:
         time.sleep(0.5)
         if boolean:
-            break
+            return 0
         x = 2*x
         
 
 class Test(Thread):
     def __init__(self):
+        super().__init__()
         t1=Thread(target=Double)
         t2=Thread(target=self.run)
         t1.start()
         time.sleep(0.25)
         t2.start()
+
 
     def run(self):
         global boolean
@@ -29,7 +31,7 @@ class Test(Thread):
                 boolean=True
                 break
             time.sleep(0.5)
-        print(x)
+
 
 loop = Test()
 loop.start()
